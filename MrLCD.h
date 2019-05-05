7137e082453a67405b855577203ede50550d6db0
ViktorMaximilian-H <35173540+ViktorMaximilian-H@users.noreply.github.com>
2019-05-05 14:07:39 +0200

    Update MrLCD.h

--- MrLCD.h
+++ MrLCD.h
@@ -5,13 +5,13 @@
 #include <util/delay.h>
 #include <stdlib.h>
 
-#define MrLCDsCrib				PORTB
-#define DataDir_MrLCDsCrib		DDRB
-#define MrLCDsControl			PORTD
-#define DataDir_MrLCDsControl	DDRD
-#define LightSwitch				5
-#define ReadWrite				7
-#define BiPolarMood				2
+#define MrLCDsCrib                PORTB
+#define DataDir_MrLCDsCrib        DDRB
+#define MrLCDsControl             PORTD
+#define DataDir_MrLCDsControl     DDRD
+#define LightSwitch               5
+#define ReadWrite                 7
+#define BiPolarMood               2
 
 void Check_IF_MrLCD_isBusy(void);
 void Peek_A_Boo(void);
